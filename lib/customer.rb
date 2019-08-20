ca30ab779ef82a8bc0a60c14261ba602665317a5
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-20 19:18:39 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,21 @@
 class Customer
   
+  attr_accessor :name, :age 
+  
+  @@all = []
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self 
+    
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.new_meal(waiter, total, tip)
+    customer.waiter = self 
+  end
+  
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
 
+
+  @@all =[]
+  def initialize(name, years_exp)
+    @name = name 
+    @years_exp = years_exp
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end
+
 end
